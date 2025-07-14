ba62f1a67c9903857a240eab1e359a5576f7a072
Prashant Aparajeya <p.aparajeya@gmail.com>
2025-07-14 09:43:54 +0000

    feat(Python): Added a sample subtraction python program

---
+++ main.py
@@ -0,0 +1,9 @@
+def sub(a, b):
+    """Subtracts two numbers."""
+    return a - b
+
+if __name__ == "__main__":
+    num1 = 10
+    num2 = 5
+    result = sub(num1, num2)
+    print(f"The result of subtracting {num2} from {num1} is: {result}")
